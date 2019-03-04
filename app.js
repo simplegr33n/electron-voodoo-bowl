4d722b9344ebad9ff36083fa1e1b841fbf95afe1
Geoff Golda <gcgolda@gmail.com>
2019-03-04 13:50:52 -0700

    adjusted package.json for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'));
 
@@ -8,6 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000! \n CTRL + C to end')
 })
